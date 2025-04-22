6ed965d2a2a3d0accc4d103dbbf7acc94631aae8
lmProgramming <mikikubs@gmail.com>
2025-04-22 13:31:52 +0200

    feat: implement CRUD operations for invoices with form handling and data fetching

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -6,7 +6,6 @@ import { LatestInvoice } from "@/app/lib/definitions";
 import { fetchLatestInvoices } from "@/app/lib/data";
 export default async function LatestInvoices() {
   const latestInvoices = await fetchLatestInvoices();
-  await new Promise((resolve) => setTimeout(resolve, 5000));
 
   return (
     <div className="flex w-full flex-col md:col-span-4">
